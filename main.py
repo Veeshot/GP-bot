ffe25e7983b45915bc0cf33659504d89222d07df
Veeshot <2matabe@gmail.com>
2022-08-29 21:16:12 +0200

    console output added

--- main.py
+++ main.py
@@ -14,5 +14,6 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+        print("answered")
 
 client.run(os.getenv('TOKEN'))
