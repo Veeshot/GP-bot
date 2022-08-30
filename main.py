f9c1fdb75ec114576a4ac2b10f4414b0345bfc06
Veeshot <106319909+Veeshot@users.noreply.github.com>
2022-08-30 20:26:11 +0000

    publishing messages every 10 minutes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from asyncio import tasks
-import discord, os, asyncio
+import discord, os
+from discord.ext import tasks
 from dotenv import load_dotenv
 from replit import db
 
@@ -14,8 +14,9 @@ client = discord.Client(intents=intents)
 
 @client.event 
 async def on_ready(): #co se má stát v moment co je bot připojen a připraven
+    publish.start() #spustí loop s funkcí publish
     print('{0.user} is now online'.format(client))
-
+    
 @client.event
 async def on_message(message): #co se má stát, že někdo odešle zprávu
     if message.author != client.user: #zajišťuje, že bot nereaguje na své zprávy
@@ -25,14 +26,13 @@ async def on_message(message): #co se má stát, že někdo odešle zprávu
             except discord.errors.Forbidden:
                 pass
             db["přiznání"].append(message.content)
-
-@tasks.loop(seconds=10) #kód v bloku by se měl opakovat každých 10 sekund
+          
+@tasks.loop(minutes = 10) #kód v bloku by se měl opakovat každých 10 sekund
 async def publish():
     if len(db["přiznání"]) != 0:
         channel = client.get_channel(971114306601107536) #objekt channel, s id kanálu #test-room - později se změní na #přiznání
         await channel.send(db["přiznání"][0]) #odeslání textu zprávy do správného kanálu
         db["přiznání"].pop(0)
-        print("succesfully published")
 
 load_dotenv() #načtení tokenu z .env
 client.run(os.getenv('TOKEN'))
