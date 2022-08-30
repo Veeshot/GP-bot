89d6aee2f16de443e52762dd91d2fd40957b716a
Veeshot <2matabe@gmail.com>
2022-08-30 12:28:33 +0200

    Login message uppdated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ client = discord.Client(intents=discord.Intents.default())
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('{0.user} is now online'.format(client))
 
 @client.event
 async def on_message(message):
