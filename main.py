adc8b079c9990802f39344f1e3273270ec228541
Veeshot <2matabe@gmail.com>
2022-08-30 19:06:48 +0200

    coping messages from DMs into channel

--- main.py
+++ main.py
@@ -2,23 +2,26 @@ import discord
 from dotenv import load_dotenv
 import os
 
-intents = discord.Intents.default()
+intents = discord.Intents.default() #oprávnění bota
 intents.message_content = True
 intents.messages = True
 
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
+@client.event 
+async def on_ready(): #co se má stát v moment co je bot připojen a připraven
     print('{0.user} is now online'.format(client))
 
 @client.event
-async def on_message(message):
-    if message.content.startswith('/hello'):
-        await message.channel.send('Hello!')
-        print("answered")
-    elif message.author == client.user:
-        return
+async def on_message(message): #co se má stát, že někdo odešle zprávu
+    if message.author != client.user: #zajišťuje, že bot nereaguje na své zprávy
+        if not message.guild: #omezuje přijaté zprávy, na které bot bude reagovat, pouze na zprávy v jeho DMs
+            try:
+                await message.channel.send("Díky za přiznání, za chvíli ho zveřejním")
+            except discord.errors.Forbidden:
+                pass
+            channel = client.get_channel(971114306601107536) #objekt channel, s id kanálu #test-room - později se změní na #přiznání
+            await channel.send(message.content) #odeslání textu zprávy do správného kanálu
 
-load_dotenv()
+load_dotenv() #načtení tokenu z .env
 client.run(os.getenv('TOKEN'))
