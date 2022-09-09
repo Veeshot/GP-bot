4c0f26650f9d56de22bae80a0731d7f53795cff9
Veeshot <106319909+Veeshot@users.noreply.github.com>
2022-09-09 20:48:55 +0000

    ?yearup command added

--- db.py
+++ db.py
@@ -2,3 +2,5 @@ from replit import db
 #file used to control database
 print(db["přiznání"])
 print(len(db["přiznání"]))
+#launch přes konzoli - *python db.py*
+#případě, že bot přestane běžet, protože ho discord zablokoval -> *kill 1* do konzole
--- main.py
+++ main.py
@@ -6,34 +6,41 @@ from replit import db
 if "přiznání" not in db.keys():
     db["přiznání"] = []
 
-intents = discord.Intents.default() #oprávnění bota
+intents = discord.Intents.default()  #oprávnění bota
 intents.message_content = True
 intents.messages = True
 
 client = discord.Client(intents=intents)
 
-@client.event 
-async def on_ready(): #co se má stát v moment co je bot připojen a připraven
-    publish.start() #spustí loop s funkcí publish
-    print('{0.user} is now online'.format(client))
-    print('There are currently {0} messages pending to be published'.format(len(db["přiznání"])))
-    
 @client.event
-async def on_message(message): #co se má stát, že někdo odešle zprávu
-    if message.author != client.user: #zajišťuje, že bot nereaguje na své zprávy
-        if not message.guild: #omezuje přijaté zprávy, na které bot bude reagovat, pouze na zprávy v jeho DMs
+async def on_ready():  #co se má stát v moment co je bot připojen a připraven
+    publish.start()  #spustí loop s funkcí publish
+    print('{0.user} is now online'.format(client))
+    print('There are currently {0} messages pending to be published'.format(
+        len(db["přiznání"])))
+
+@client.event
+async def on_message(message):  #co se má stát, že někdo odešle zprávu
+    if message.author != client.user:  #zajišťuje, že bot nereaguje na své zprávy
+        if not message.guild:  #omezuje přijaté zprávy, na které bot bude reagovat, pouze na zprávy v jeho DMs
             try:
-                await message.channel.send("Díky za přiznání, za chvíli ho zveřejním")
+                await message.channel.send(
+                    "Díky za přiznání, za chvíli ho zveřejním")
             except discord.errors.Forbidden:
                 pass
             db["přiznání"].append(message.content)
-          
-@tasks.loop(minutes = 10) #kód v bloku by se měl opakovat každých 10 sekund
+		elif message.content == "?yearup":
+			pass
+
+@tasks.loop(seconds=10)  #kód v bloku by se měl opakovat každých 10 sekund
 async def publish():
     if len(db["přiznání"]) != 0:
-        channel = client.get_channel(971114306601107536) #objekt channel, s id kanálu #test-room - později se změní na #přiznání
-        await channel.send(db["přiznání"][0]) #odeslání textu zprávy do správného kanálu
+        channel = client.get_channel(
+            1014200735212249088
+        )  #objekt channel, s id kanálu #test-room - později se změní na #přiznání
+        await channel.send(db["přiznání"][0]
+                           )  #odeslání textu zprávy do správného kanálu
         db["přiznání"].pop(0)
 
-load_dotenv() #načtení tokenu z .env
+load_dotenv()  #načtení tokenu z .env
 client.run(os.getenv('TOKEN'))
